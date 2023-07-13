00736d559cd7b98ec740569c2001afbc417ee041
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-13 07:10:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,6 @@ class Config:
     FSUB = getenv("FSUB", " ")
     CHID = int(getenv("CHID", "  "))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Advika:advika@cluster0.tvgwdkv.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
